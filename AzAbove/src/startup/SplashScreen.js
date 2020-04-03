d980f140dac201da58e21704a2252c0908fdfc48
Sheikh Rahman <sheikh1906@gmail.com>
2020-04-03 00:16:54 -0500

    Navigation

--- AzAbove/src/startup/SplashScreen.js
+++ AzAbove/src/startup/SplashScreen.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const SplashScreen = props => {
+
+    const { navigate } = props.navigation;
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('Skymap');
+        }, 4000)
+    },[])
+
     return (
         <View >
             <Text>Splash Screen</Text>
